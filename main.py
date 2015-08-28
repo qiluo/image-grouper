d0b792e9a22124b180a6b5825555fa35ef898705
qi.luo <qi.luo@philips.com>
2015-08-28 08:27:50 +0800

    read date from exif by exifread

--- main.py
+++ main.py
@@ -25,6 +25,34 @@ Options:
 from docopt import docopt
 import os
 from logger import Logger
+import exifread
+
+# what tags use to redate file (use first found)
+DT_TAGS = ["Image DateTime", "EXIF DateTimeOriginal", "DateTime"]
+
+def readExifDate(image_path):
+    """read date from exif, return in format of ('2005:10:20 23:22:28'),
+    return None if any error"""
+    f = open(image_path, 'rb')
+    try:
+        tags = exifread.process_file(f, details=False)
+        for dt_tag in DT_TAGS:
+            try:
+                dt_value = '%s' % tags[dt_tag]
+                logger.info('%s: %s' % (dt_tag, dt_value))
+            except:
+                continue
+
+        if dt_value:
+            return dt_value
+    finally:
+        f.close()
+
+    return None
+
+
+def handleImageByDate(imagePath, date, shouldMove, outputDir):
+    pass
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='0.0.1')
@@ -36,13 +64,9 @@ if __name__ == '__main__':
         for filename in filenames:
             logger.info(os.sep.join([dirpath, filename]))
             images.extend(os.sep.join([dirpath, filename]))
+            date = readExifDate(os.sep.join([dirpath, filename]))
+            logger.info(date)
 
         # no recurse, just top level dir
         if not args['-r']:
             break
-
-def readExif(image_path):
-    pass
-
-def handleImageByDate(imagePath, date, shoudMove, outputDir):
-    pass
